5e5b645355ef173350d9d39915c4924f8612f866
DOCENTE - JEMSUN ALEXIS TAMAYO TELLO <D19120@idat.edu.pe>
2019-11-26 21:05:19 -0500

    first commit

---
+++ Semana11/src/main/java/aplicaciones/spring/Semana11Application.java
@@ -0,0 +1,13 @@
+package aplicaciones.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Semana11Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Semana11Application.class, args);
+	}
+
+}
